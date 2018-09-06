45b95499ff5c9e979a1ac6837fa4dae0496feb12
cladder <1241818518@qq.com>
2018-09-06 23:28:23 +0800

    u

--- app/service/guest.js
+++ app/service/guest.js
@@ -37,7 +37,7 @@ class GuestService extends Service {
       // #表示不跳转
       href: '#',
       // pic: 'https://cdn.520cy.cn/images/appbazaar.png',
-      pic: 'http://ugc.qpic.cn/gbar_pic/2wF3sr2LiaVtQE337kOWdsAKFuc2y57haIiborP8yNQ2ypXric7RS0rOg/0',
+      pic: '//ugc.qpic.cn/gbar_pic/2wF3sr2LiaVtQE337kOWdsAKFuc2y57haIiborP8yNQ2ypXric7RS0rOg/0',
       alt: '千寻软件市场'
     };
   }
